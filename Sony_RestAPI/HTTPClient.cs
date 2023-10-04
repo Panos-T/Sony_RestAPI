c672b6da6f9b85d62b3313200869d4fbc645f9b7
Panos-AI <91608320+Panos-AI@users.noreply.github.com>
2023-10-04 18:04:42 +0300

    Add project files.

---
+++ Sony_RestAPI/HTTPClient.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Crestron.SimplSharp.Net.Http;
+using Crestron.SimplSharp;
+
+namespace Sony_RestAPI
+{
+    
+    internal class HTTPClient : HttpClient
+    {
+        private string IPaddress, PSK;
+
+
+
+
+
+        /*
+         * Constructor
+         */
+
+
+        public HTTPClient(string address, string psk)
+        {
+            this.IPaddress = address;
+            this.PSK = psk;
+
+        }
+
+       
+
+        private HttpClientRequest CreateRequest(RequestType type, string message, string body = null, HttpHeader[] headers = null)
+        {
+            HttpClientRequest request = new HttpClientRequest
+            {
+                Url = new UrlParser("http://" + IPaddress + "/sony" + message),
+                RequestType = type
+            };
+           
+            //Create the authentication Header
+            request.Header.SetHeaderValue("X-Auth-PSK", PSK);
+            
+
+            //If any Headers are specified, include them as well
+            if (headers != null)
+            {
+                for (int i = 0; i < headers.Length; i++)
+                {
+                    request.Header.AddHeader(headers[i]);
+                }
+            }
+
+            //If a body is specified, include it
+            if (body != null)
+            {
+                request.ContentString = body;
+            }
+
+            return request;
+        }
+
+
+        private HttpClientResponse DispatchRequest(HttpClientRequest request, HTTPClientResponseCallback callback = null)
+        {
+            HttpClientResponse response;
+            HTTP_CALLBACK_ERROR error;
+
+            try
+            {
+                GlobalFunctions.PrintDeb("Headers {{\n" + request.Header + "\n}}");
+                response = Dispatch(request);
+                GlobalFunctions.PrintDeb("Success Header " + request.Header);
+                error = HTTP_CALLBACK_ERROR.COMPLETED;
+            }
+            catch(Exception e)
+            {
+                GlobalFunctions.PrintDeb("Error sending the request");
+                GlobalFunctions.PrintDeb("Error Type \n {{" + e + "\n}}");
+                GlobalFunctions.PrintDeb("Error Inner Exception " + e.InnerException);
+                GlobalFunctions.PrintDeb("Error Hresult " + e.HResult);
+                GlobalFunctions.PrintDeb("Error sending the request " + e.Message);
+                GlobalFunctions.PrintDeb("Error Header " + request.Header);
+
+
+
+                response = null;
+                error = HTTP_CALLBACK_ERROR.UNKNOWN_ERROR;
+                
+            }
+
+            callback?.Invoke(response, error);
+            return response;
+        }
+
+
+
+        public HttpClientResponse SendHttpRequest(RequestType type, string message, string body = null, HttpHeader[] headers = null)
+        {
+
+    
+            
+            HttpClientRequest request = CreateRequest(type, message, body, headers);
+            
+            
+            
+            
+
+            return DispatchRequest(request);
+        }
+
+
+   
+
+
+
+
+
+    }
+}
---
+++ Sony_RestAPI/Sony_RestAPI_Client.cs
@@ -0,0 +1,108 @@
+﻿using Crestron.SimplSharp;
+using Crestron.SimplSharp.Net.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sony_RestAPI
+{
+    public class Sony_RestAPI_Client
+    {
+        private HTTPClient client ;
+        private int retryFactor;
+
+
+
+        public abstract class SonyBraviaIPMethod<T>
+        {
+            public string method { get; set; }
+            public int id { get; set; }
+            public T[] @params { get; set; }
+            public string version { get; set; }
+        }
+
+
+        internal class Uri
+        {
+            public string uri;
+        }
+
+        internal sealed class setActiveApp : SonyBraviaIPMethod<Uri>
+        {
+            public setActiveApp()
+            {
+                method = "setActiveApp";
+                id = 601;
+                @params = new Uri[1];
+                @params[0] = new Uri();
+                version = "1.0";
+            }
+        }
+
+
+        /*
+         * Function called at SIMPL+ startup
+         */
+
+        public void Initialize(string iptxt, string psktxt)
+        {
+            client = new HTTPClient(iptxt, psktxt);
+            setDebugLevel(0);
+            
+        }
+
+
+        public void selectApp(string Uri)
+        {
+
+            setActiveApp requestBody = new setActiveApp();
+
+            requestBody.@params[0].uri = Uri;
+
+
+            GlobalFunctions.PrintDeb("requestBody: " + JsonConvert.SerializeObject(requestBody));
+            //client.SendHttpRequestAsync(RequestType.Post, "/appControl", JsonConvert.SerializeObject(requestBody), new HttpHeader[1] { new HttpHeader("Content-Type", "application/json;") });
+            HttpClientResponse response = client.SendHttpRequest(RequestType.Post, "/appControl", JsonConvert.SerializeObject(requestBody), new HttpHeader[1] { new HttpHeader("Content-Type", "application/json; charset=UTF-8") });
+
+
+            
+            if (response == null && retryFactor < 5)
+            {
+                retryFactor++;
+                GlobalFunctions.PrintDeb("Http Request retry attempt No " + retryFactor);
+                this.selectApp(Uri);
+            }
+            else
+            {
+                GlobalFunctions.PrintDeb("Http Request Succeeded after " + retryFactor + " retries");
+                GlobalFunctions.PrintDeb("Response Content " + response.ContentString);
+                GlobalFunctions.PrintDeb("Response Code " + response.Code);
+                retryFactor = 0;
+
+            }
+        }
+
+        public void setDebugLevel(ushort lvl)
+        {
+            GlobalFunctions.DebugLevel = lvl;
+        }
+
+
+
+    }
+    public static class GlobalFunctions
+    {
+        public static int DebugLevel;
+        public static void PrintDeb(string msg)
+        {
+            if (DebugLevel != 0)
+            {
+                CrestronConsole.PrintLine(msg);
+            }
+        }
+    }
+   
+}
